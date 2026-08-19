8a59b53d3d33712ce10568f9dece224286f0f316
Sato Gojo <gojo.lvl99@gmail.com>
2026-08-19 09:08:47 +0000

    Bump version to 2.3.6

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.relay7.core"
         minSdk = 24
         targetSdk = 37
-        versionCode = 743
-        versionName = "2.3.3"
+        versionCode = 744
+        versionName = "2.3.6"
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
         splits {
